d8771bd727add15eae0ef7faf1be7415e700c6ee
Marco Cordeiro <marco.cordeiro@insurwave.com>
2021-12-07 20:42:00 +0000

    Solved day 6

--- src/DailyChallenges/Day06/ChallengeDay6.cs
+++ src/DailyChallenges/Day06/ChallengeDay6.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using AdventOfCode2020.Framework;
 using DataProvider;
 
@@ -23,19 +25,97 @@ namespace AdventOfCode2020.DailyChallenges.Day06
         {
             _output.WriteLine($"Advent of Code day {Day}");
 
-            ResolvePart1();
-            ResolvePart2();
+            var data = _dataProvider.Read(Day).First().AsLanternFishPopulation().ToArray();
+            ResolvePart1(data);
+            ResolvePart2(data);
         }
 
-        private void ResolvePart1()
+        private void ResolvePart1(IEnumerable<int> data)
         {
-            _output.WriteLine($"\tPart 1 is not available");
+            var population = new LanternFishPopulation(data);
+            
+            population.Evolve(80);
+            
+            _output.WriteLine($"\tThere should be {population.PopulationCount} lantern fish after 80 days");
         }
 
-        private void ResolvePart2()
+        private void ResolvePart2(IEnumerable<int> data)
         {
-            _output.WriteLine($"\tPart 2 is not available");
+            var population = new LanternFishPopulation(data);
+            
+            population.Evolve(256);
+            
+            _output.WriteLine($"\tThere should be {population.PopulationCount} lantern fish after 256 days");
         }
 
+    }
+
+    public class LanternFishPopulation
+    {
+        private Dictionary<int, long> _population;
+
+        public LanternFishPopulation(IEnumerable<int> population)
+        {
+            _population = population.ToLookup(x => x).ToDictionary(x => x.Key, x => (long)x.Count());
+        }
+
+        public long PopulationCount => _population.Sum(x=>x.Value);
+
+        public void Evolve(int days)
+        {
+            for (var i = 0; i < days; i++)
+            {
+                Evolve();
+            }
+        }
+
+        public void Evolve()
+        {
+            var nextDayPopulation = _population.ToDictionary(x => x.Key - 1, x => x.Value);
+            nextDayPopulation.TryGetValue(-1, out var expectantSpecimen);
+            nextDayPopulation.TryGetValue(6, out var daySixSpecimen);
+            nextDayPopulation.Remove(-1);
+            nextDayPopulation[6] = daySixSpecimen + expectantSpecimen;
+            nextDayPopulation[8] = expectantSpecimen;
+
+            _population = nextDayPopulation;
+        }
+    }
+    
+    public static class ChallengeDay6Extensions
+    {
+        public static IEnumerable<int> AsLanternFishPopulation(this string data)
+        {
+            return data.Split(',').Select(int.Parse);
+        }
+
+        public static void Evolve(this IList<int> population, int days)
+        {
+            for (var i = 0; i < days; i++)
+            {
+                population.Evolve();
+            }
+        }
+
+        public static void Evolve(this IList<int> population)
+        {
+            var currentPopulation = population.Count;
+            var newPopulation = new ConcurrentBag<byte>();
+                
+            Parallel.For(0, currentPopulation, i =>
+            {
+                var specimen = population[i]--;
+                if (specimen == 0)
+                {
+                    population[i] = 6;
+                    newPopulation.Add(8);
+                }
+            });
+
+            foreach (var specimen in newPopulation)
+            {
+                population.Add(specimen);    
+            }
+        }
     }
 }
--- test/DailyChallengesTests/ChallengeDay06Tests.cs
+++ test/DailyChallengesTests/ChallengeDay06Tests.cs
@@ -1,4 +1,7 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2020.DailyChallenges.Day06;
 using FluentAssertions;
 using Xunit;
 
@@ -7,14 +10,89 @@ namespace DailyChallengesTests
     public class ChallengeDay06Tests
     {
         [Fact]
-        public void Should_test_day_challenge_logic()
+        public void Population_should_evolve()
         {
-            Assert.True(false, "day challenge not implemented yet");
+            var population = SampleData.First().AsLanternFishPopulation().ToList();
+            
+            foreach (var state in ExpectedEvolution)
+            {
+                population.Evolve();
+                population.Should().BeEquivalentTo(state.AsLanternFishPopulation());
+            }
         }
         
+        [Fact]
+        public void Population_should_evolve_80_days()
+        {
+            var population = SampleData.First().AsLanternFishPopulation().ToList();
+            
+            population.Evolve(80);
+
+            population.Count.Should().Be(5934);
+        }
+        
+        [Fact]
+        public void Population_should_evolve_80_days_two()
+        {
+            var data = SampleData.First().AsLanternFishPopulation();
+            var population = new LanternFishPopulation(data);
+            
+            population.Evolve(80);
+            
+            population.PopulationCount.Should().Be(5934);
+        }
+        
+        [Fact]
+        public void Population_should_evolve_18_days_two()
+        {
+            var data = SampleData.First().AsLanternFishPopulation();
+            var population = new LanternFishPopulation(data);
+            
+            population.Evolve(18);
+            
+            population.PopulationCount.Should().Be(26);
+        }
+        
+        [Fact]
+        public void Population_should_evolve_256_days()
+        {
+            var data = SampleData.First().AsLanternFishPopulation();
+            var population = new LanternFishPopulation(data);
+            
+            population.Evolve(256);
+            
+            population.PopulationCount.Should().Be(26984457539);
+        }
+
         private IEnumerable<string> SampleData
         {
-            get { yield return "value"; }
+            get { yield return "3,4,3,1,2"; }
         }
+
+        private IEnumerable<string> ExpectedEvolution
+        {
+            get
+            {
+                yield return "2,3,2,0,1";
+                yield return "1,2,1,6,0,8";
+                yield return "0,1,0,5,6,7,8";
+                yield return "6,0,6,4,5,6,7,8,8";
+                yield return "5,6,5,3,4,5,6,7,7,8";
+                yield return "4,5,4,2,3,4,5,6,6,7";
+                yield return "3,4,3,1,2,3,4,5,5,6";
+                yield return "2,3,2,0,1,2,3,4,4,5";
+                yield return "1,2,1,6,0,1,2,3,3,4,8";
+                yield return "0,1,0,5,6,0,1,2,2,3,7,8";
+                yield return "6,0,6,4,5,6,0,1,1,2,6,7,8,8,8";
+                yield return "5,6,5,3,4,5,6,0,0,1,5,6,7,7,7,8,8";
+                yield return "4,5,4,2,3,4,5,6,6,0,4,5,6,6,6,7,7,8,8";
+                yield return "3,4,3,1,2,3,4,5,5,6,3,4,5,5,5,6,6,7,7,8";
+                yield return "2,3,2,0,1,2,3,4,4,5,2,3,4,4,4,5,5,6,6,7";
+                yield return "1,2,1,6,0,1,2,3,3,4,1,2,3,3,3,4,4,5,5,6,8";
+                yield return "0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8";
+                yield return "6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8";
+            }
+        }
+
     }
 }
