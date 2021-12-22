74af02faac5349e345ce4e9179d19b16c22e3074
Marco Cordeiro <marco.cordeiro@insurwave.com>
2021-12-22 22:03:36 +0000

    solved day 10

--- src/DailyChallenges/Day10/ChallengeDay10.cs
+++ src/DailyChallenges/Day10/ChallengeDay10.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AdventOfCode2020.Framework;
 using DataProvider;
 
@@ -6,10 +9,10 @@ namespace AdventOfCode2020.DailyChallenges.Day10
 {
     public class ChallengeDay10 : IAdventCodeDayChallenge
     {
-        private readonly IDataProvider<int> _dataProvider;
+        private readonly IDataProvider<string> _dataProvider;
         private readonly TextWriter _output;
 
-        public ChallengeDay10(IDataProvider<int> dataProvider, TextWriter output)
+        public ChallengeDay10(IDataProvider<string> dataProvider, TextWriter output)
         {
             _dataProvider = dataProvider;
             _output = output;
@@ -21,18 +24,138 @@ namespace AdventOfCode2020.DailyChallenges.Day10
         {
             _output.WriteLine($"Advent of Code day {Day}");
 
-            ResolvePart1();
-            ResolvePart2();
+            var data = _dataProvider.Read(Day).ToArray();
+            
+            ResolvePart1(data);
+            ResolvePart2(data);
         }
 
-        private void ResolvePart1()
+        private void ResolvePart1(IEnumerable<string> code)
         {
-            _output.WriteLine($"\tPart 1 is not available");
+            var errors = code.CheckCodeSyntax();
+            var result = errors.ScoreSyntaxErrors();
+            _output.WriteLine($"\tThe score of all syntax errors is {result}");
         }
 
-        private void ResolvePart2()
+        private void ResolvePart2(IEnumerable<string> code)
         {
-            _output.WriteLine($"\tPart 2 is not available");
+            var errors = code.CheckCodeSyntax();
+            var result = errors.ScoreAutoComplete();
+            _output.WriteLine($"\tThe score of auto complete code is {result}");
         }
     }
+    
+    public static class ChallengeDay10Extensions
+    {
+        public static int ScoreSyntaxErrors(this IEnumerable<SyntaxStatus> errors)
+        {
+            var scoreMap = new Dictionary<char, int>
+            {
+                [')'] = 3,
+                [']'] = 57,
+                ['}'] = 1197,
+                ['>'] = 25137,
+            };
+            return errors.Where(x => !x.IsValid).Sum(x => scoreMap[x.UnexpectedChar]);
+        }
+
+        public static long ScoreSyntaxAutoComplete(this SyntaxStatus status)
+        {
+            var scoreMap = new Dictionary<char, int>
+            {
+                [')'] = 1,
+                [']'] = 2,
+                ['}'] = 3,
+                ['>'] = 4
+            };
+            long score = 0;
+            foreach (var @char in status.AutoComplete)
+            {
+                score *= 5;
+                score += scoreMap[@char];
+            }
+
+            return score;
+        }
+        
+        public static long ScoreAutoComplete(this IEnumerable<SyntaxStatus> status)
+        {
+            var code = status.Where(x => x.IsValid && !string.IsNullOrEmpty(x.AutoComplete));
+            var scores = code.Select(x => x.ScoreSyntaxAutoComplete()).OrderBy(x => x).ToArray();
+            return scores[scores.Length / 2];
+        }
+
+        public static IEnumerable<SyntaxStatus> CheckCodeSyntax(this IEnumerable<string> code)
+        {
+            return code.Select(line => line.IsSyntaxValid());
+        }
+
+        public static SyntaxStatus IsSyntaxValid(this string code)
+        {
+            var map = new Dictionary<char, char>
+            {
+                ['<'] = '>',
+                ['{'] = '}',
+                ['('] = ')',
+                ['['] = ']',
+            };
+            var blocks = new Stack<char>();
+            foreach (var @char in code)
+            {
+                switch (@char)
+                {
+                    case '<':
+                    case '(':
+                    case '{':
+                    case '[':
+                        blocks.Push(@char);
+                        break;
+                    case '>':
+                    case ')':
+                    case '}':
+                    case ']':
+                        var expectedChar = blocks.Pop();
+                        if (@char != map[expectedChar])
+                        {
+                            return new SyntaxStatus(@char);
+                        }
+                        break;
+                    default:
+                        throw new ArgumentException(@char.ToString());
+                }
+            }
+
+            var autoComplete = new string(blocks.ToArray().Select(x=>map[x]).ToArray());
+            return new SyntaxStatus(autoComplete);
+        }
+    }
+
+    public class SyntaxStatus
+    {
+        public SyntaxStatus()
+        {
+            IsValid = true;
+            UnexpectedChar = '\0';
+            AutoComplete = string.Empty;
+        }
+        
+        public SyntaxStatus(string autoComplete)
+        {
+            IsValid = true;
+            UnexpectedChar = '\0';
+            AutoComplete = autoComplete;
+        }
+        
+        public SyntaxStatus(char unexpectedChar)
+        {
+            IsValid = false;
+            UnexpectedChar = unexpectedChar;
+            AutoComplete = string.Empty;
+        }
+
+        public bool IsValid { get; }
+        public char UnexpectedChar { get; }
+        public string AutoComplete { get; }
+    }
+
 }
--- test/DailyChallengesTests/ChallengeDay10Tests.cs
+++ test/DailyChallengesTests/ChallengeDay10Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using AdventOfCode2020.DailyChallenges.Day10;
 using FluentAssertions;
 using Xunit;
 
@@ -7,14 +8,75 @@ namespace DailyChallengesTests
     public class ChallengeDay10Tests
     {
         [Fact]
-        public void Should_test_day_challenge_logic()
+        public void Should_find_syntax_error()
         {
-            Assert.True(false, "day challenge not implemented yet");
+            var code = InvalidSampleData;
+            var result = code.IsSyntaxValid();
+            result.IsValid.Should().Be(false);
+            result.UnexpectedChar.Should().Be('}');
+        }
+        
+        [Fact]
+        public void Should_ignore_incomplete_code()
+        {
+            var code = IncompleteSampleData;
+            var result = code.IsSyntaxValid();
+            result.IsValid.Should().Be(true);
+            result.AutoComplete.Should().Be("}}]])})]");
+            result.UnexpectedChar.Should().Be('\0');
+        }
+        
+        [Fact]
+        public void Should_account_syntax_errors_score()
+        {
+            var errors = SampleData.CheckCodeSyntax();
+            var result = errors.ScoreSyntaxErrors();
+            
+            result.Should().Be(26397);
+        }
+        
+        [Fact]
+        public void Should_account_autocomplete_score()
+        {
+            var status = new SyntaxStatus("])}>");
+            var result =status.ScoreSyntaxAutoComplete();
+            
+            result.Should().Be(294);
+        }
+        
+        [Fact]
+        public void Should_account_autocomplete_score2()
+        {
+            var code = IncompleteSampleData;
+            var result = code.IsSyntaxValid().ScoreSyntaxAutoComplete();
+            result.Should().Be(288957);
+        }
+        
+        [Fact]
+        public void Should_account_autocomplete_scores()
+        {
+            var status = SampleData.CheckCodeSyntax();
+            var result = status.ScoreAutoComplete();
+            
+            result.Should().Be(288957);
         }
 
+        private string IncompleteSampleData => "[({(<(())[]>[[{[]{<()<>>";
+        private string InvalidSampleData => "{([(<{}[<>[]}>{[]{[(<()>";
         private IEnumerable<string> SampleData
         {
-            get { yield return "value"; }
+            get { 
+                yield return "[({(<(())[]>[[{[]{<()<>>";
+                yield return "[(()[<>])]({[<{<<[]>>(";
+                yield return "{([(<{}[<>[]}>{[]{[(<()>";
+                yield return "(((({<>}<{<{<>}{[]{[]{}";
+                yield return "[[<[([]))<([[{}[[()]]]";
+                yield return "[{[{({}]{}}([{[{{{}}([]";
+                yield return "{<[[]]>}<{[{[{[]{()[[[]";
+                yield return "[<(<(<(<{}))><([]([]()";
+                yield return "<{([([[(<>()){}]>(<<{{";
+                yield return "<{([{{}}[<[[[<>{}]]]>[]]";
+            }
         }
     }
 }
